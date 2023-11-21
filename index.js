2356214bb3de1cf81a7750be98d22f1bb0a3dc12
suraj <surajjosh7@gmail.com>
2023-11-21 07:54:42 +0530

    new feature click

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const convert= require('./src/converter');
 const play= require('./src/play');
 const click = require('./src/click_i')
 
-click(1000,500,0,"suraj.jpg");
+
 
 module.exports={convert,play,click};
